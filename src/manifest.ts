d99cb4ca31cb8b076b3640803a8f0170dbc7c25a
Kashav Madan <kshvmdn@gmail.com>
2024-08-25 12:45:12 -0400

    Prepare for Chrome store upload
    Disabled icon files temporarily due to #4.
    
    See #4.

--- src/manifest.ts
+++ src/manifest.ts
@@ -10,10 +10,10 @@ export default defineManifest({
   version: packageData.version,
   manifest_version: 3,
   icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
+    // 16: 'img/logo-16.png',
+    // 32: 'img/logo-34.png',
+    // 48: 'img/logo-48.png',
+    // 128: 'img/logo-128.png',
   },
   background: {
     service_worker: 'src/background/index.ts',
@@ -27,9 +27,9 @@ export default defineManifest({
   ],
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: [/* 'img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png' */],
       matches: [],
     },
   ],
-  permissions: ['activeTab', 'contextMenus', 'scripting'],
+  permissions: ['activeTab', 'contextMenus'],
 })
